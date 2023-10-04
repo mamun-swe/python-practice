b02055f16e7dac9be5a711a3241f4609c32ffc8a
mamun-swe <mamun.swe.277@gmail.com>
2023-10-04 17:31:07 +0600

    Variable types completed

---
+++ variable.py
@@ -0,0 +1,16 @@
+myInt = 7
+myFloat = 7.028
+myString = "This is string!"
+print(myInt)
+print(myFloat)
+print(myString)
+
+hello = "Hello"
+world = "World"
+helloWorld = hello + " " + world
+print(helloWorld)
+
+a, b = 3, 4
+print(a, b)
+
+# print(myInt + myFloat + myString)  # This will not work for mixing
