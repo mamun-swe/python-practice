4f910c463ed6ee76c876501b07f32e664ec7dc72
mamun-swe <mamun.swe.277@gmail.com>
2023-10-08 12:34:41 +0600

    String practice added

---
+++ strings.py
@@ -0,0 +1,15 @@
+x = "hello 8"
+print(len(x))
+
+if "fun" in "this is fun":
+    print("available")
+
+
+name = "x"
+result = "John" if name == "John" else "Jane"
+print(result)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is %d"
+
+print(format_string % data)
