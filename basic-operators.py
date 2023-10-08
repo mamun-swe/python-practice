e3776630ecf82d6bf2ce4e6943c167165b0f276d
mamun-swe <mamun.swe.277@gmail.com>
2023-10-08 11:38:01 +0600

    Basic operators implemented

---
+++ basic-operators.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# Modulus (%)
+reminder = 11 % 3
+print(reminder)
+
+# two multiplication
+squared = 7**2
+cubed = 2**3
+print(squared, cubed)
+
+# Operators with Strings
+helloworld = "hello" + "world"
+print(helloworld)
+
+# strings multiplication
+lotsOfStr = "hello " * 10
+print(lotsOfStr)
+
+# Operators with Lists
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# List multiplication
+print([1, 2, 3] * 3)
