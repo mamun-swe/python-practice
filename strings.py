87a30c814d925528f7523a4004840a9ad5dabd2b
mamun-swe <mamun.swe.277@gmail.com>
2023-10-08 12:45:59 +0600

    String practice added

--- strings.py
+++ strings.py
@@ -13,3 +13,24 @@ data = ("John", "Doe", 53.44)
 format_string = "Hello %s %s. Your current balance is %d"
 
 print(format_string % data)
+
+# multi-line string
+multi_line = """This is a 
+Multiline string"""
+print(multi_line)
+
+# join a string with a comma delimiter
+print(",".join(["foo", "bar"]))
+
+# split a string to a list using a comma delimiter
+print("foo,bar".split(","))
+
+# print a number using a format string
+number = 1123
+print("%d" % number)
+
+# print a number using an "f string"
+print(f"{number}")
+
+# print a number using the string format function
+print("{number}".format(number=number))
